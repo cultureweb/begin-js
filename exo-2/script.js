15f442ee59a9bf7975c5eb24b7abc3a211cd06e9
ced <cedric.milius@yahoo.fr>
2019-04-02 10:23:07 +0200

    exercice 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,32 @@
 // you can write js here
+
+var registered = false;//prompt('Etes vous inscrit ? oui ou non');
+var ageOfRunner =  prompt('Quel age avez vous ?');
+
+var raceNumber;
+
+
+if (registered === 'non') {
+    raceNumber += 1000;
+}
+else {
+    raceNumber = Math.floor(Math.random()*1000);
+   // console.log(raceNumber);
+}
+if (registered === true && ageOfRunner > 18) {
+console.log('You will race at 9:30 am');
+
+}
+else if  (registered === true || ageOfRunner > 18){
+    console.log('You will race at 11:00 am, vous avez le dossard numéro ' + raceNumber);
+    
+}
+else if (registered === false || ageOfRunner < 18){
+        console.log('You will race at 12:30 pm ' + raceNumber);         
+}
+else {
+    console.log('Go to see the registration desk');
+}
+
+
 console.log('exo-2');
