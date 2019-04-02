7e14499097dc8d6024c33fcd216c889ba10a9169
ced <cedric.milius@yahoo.fr>
2019-04-02 16:01:23 +0200

    exercice 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,21 @@
-// you can write js here
+var input ='Turpentine and turtles';
+var vowels = ['a', 'e', 'i', 'e', 'o', 'u', 'y' ]
+var resultArray =[];
+for (let i = 0; i < input.length; i++) {// input[i] renvoie le ième caractère de maChaine
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] == vowels[j]){       
+    resultArray.push(input[i]);   
+        }
+    }
+    // if (vowels.indexOf(input[i]) != -1 ) {//renvoie le premier indice du tableau. Si pas d'élément renvoie -1
+    // console.log(input[i]);
+    // }
+}
+
+console.log(resultArray.join(''));
+resultArray = resultArray.join('').toUpperCase();
+
+console.log(resultArray);
+
 
 console.log('exo-5');
