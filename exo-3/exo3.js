d40d57674376af841faffd3dfed5ba6fb796f487
ced <cedric.milius@yahoo.fr>
2019-04-04 11:03:15 +0200

    ajout d'un code secret afin d'être certain de gagner dans tous les cas

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,31 +1,31 @@
 console.log(' Bienvenue dans ce petit jeu : ');
-const choices = ['rock', 'paper','scissors'];
-for (let i = 0; i < choices.length; i++){
+const choices = ['rock', 'paper', 'scissors'];
+for (let i = 0; i < choices.length; i++) {
     console.log(i + ' : ' + choices[i]);
 }
-//var choice  = prompt("faites votre choix");
-//console.log(choice);
-function getUserChoice(userInput){
+function getUserChoice(userInput) {
     userInput = userInput.toLowerCase();
     //console.log(userInput);
-
-    switch(userInput) {
+    switch (userInput) {
+        case 'bomb':
+            console.log('Vous avez fait le choix bomb');
+            break;
         case 'rock':
-        console.log('Vous avez fait le choix rock');
-        break;
+            console.log('Vous avez fait le choix rock');
+            break;
         case 'paper':
-        console.log('Vous avez fait le choix paper');
-        break;
+            console.log('Vous avez fait le choix paper');
+            break;
         case 'scissors':
-        console.log('Vous avez fait le choix scissors');
-        break;
+            console.log('Vous avez fait le choix scissors');
+            break;
         default:
-        console.log(userInput + ' n\'est pas un choix disponible');
-        
+            console.log(userInput + ' n\'est pas un choix disponible');
+
     }
     return userInput;
 }
-function getComputerChoice(){
+function getComputerChoice() {
     let computerChoiceNumber = Math.round(Math.random() * 2);// assigne nombre entier entre 0 et 2
     //console.log(computerChoiceNumber);
     let computerChoice = choices[computerChoiceNumber];// assigne rock pour 0, paper pour 1, scissor pour 2
@@ -35,17 +35,20 @@ function getComputerChoice(){
 
 }
 function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice){
-         return console.log('Tied : égalité');
-     }
-    if (userChoice === 'rock' && computerChoice === 'scissors'){
-        return console.log('userchoice is rock, computerChoice is scissors. You won ');
-    
+    if (userChoice === 'bomb') {
+        return console.log('you win');
     }
-    else if (userChoice === 'paper' && computerChoice === 'rock'){
+    if (userChoice === computerChoice) {
+        return console.log('Tied : égalité');
+    }
+    if (userChoice === 'rock' && computerChoice === 'scissors') {
+        return console.log('userchoice is rock, computerChoice is scissors. You won ');
+
+    }
+    else if (userChoice === 'paper' && computerChoice === 'rock') {
         return console.log('userchoice is paper, computerChoice is rock. You won ');
     }
-    else if (userChoice === 'scissors' && computerChoice === 'paper'){
+    else if (userChoice === 'scissors' && computerChoice === 'paper') {
         return console.log('userchoice is paper, computerChoice is rock. You won ');
     }
     else {
@@ -53,19 +56,17 @@ function determineWinner(userChoice, computerChoice) {
     }
 }
 function playGame() {
-
-    //var uChoice =  getUserChoice(prompt("faites votre choix"));
     var choiceCleared = false;// signifie validation du choix
     do {
-        var uChoice =  getUserChoice(prompt("faites votre choix"));
-        if (uChoice === 'rock' || uChoice === 'paper' || uChoice === 'scissors'){
+        var uChoice = getUserChoice(prompt("faites votre choix"));
+        if (uChoice === 'rock' || uChoice === 'paper' || uChoice === 'scissors' || uChoice === 'bomb') {
             choiceCleared = true;
         }
-      } while (choiceCleared != true);
-   
-    
-   var computerChoice = getComputerChoice();
-   determineWinner(uChoice, computerChoice);
+    } while (choiceCleared != true);
+
+
+    var computerChoice = getComputerChoice();
+    determineWinner(uChoice, computerChoice);
 
 }
 playGame();
